4ce330b4cda911629b4641d84a3d27d23a4fd168
Humberto Paulo B. Filho <48321268+HumbertoH7@users.noreply.github.com>
2025-09-19 16:23:29 -0300

    Update SolverResult.cs

--- src/AdmissionTest.Core/Solving/SolverResult.cs
+++ src/AdmissionTest.Core/Solving/SolverResult.cs
@@ -1,11 +1,29 @@
-namespace AdmissionTest.Core.Solving;
-
+using System;
+using System.Collections.Generic;
 using AdmissionTest.Core.Models;
+
+namespace AdmissionTest.Core.Solving;
 
 public sealed record SolverResult
 {
     public required Scenario Scenario { get; init; }
     public required IReadOnlyList<int> ChosenValues { get; init; }
     public required int AchievedSum { get; init; }
-    public required int DifferenceFromTarget { get; init; }
+    public int DifferenceFromTarget => Math.Abs(AchievedSum - Scenario.TargetValue);
+
+    public static SolverResult From(Scenario scenario, IReadOnlyList<int> chosenValues)
+    {
+        if (scenario is null) throw new ArgumentNullException(nameof(scenario));
+        if (chosenValues is null) throw new ArgumentNullException(nameof(chosenValues));
+
+        var sum = 0;
+        for (int i = 0; i < chosenValues.Count; i++) sum += chosenValues[i];
+
+        return new SolverResult
+        {
+            Scenario = scenario,
+            ChosenValues = chosenValues,
+            AchievedSum = sum
+        };
+    }
 }
